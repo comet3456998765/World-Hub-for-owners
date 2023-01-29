d2381c0a820e9b371f8ecf69e93d212567795c09
comet3456998765 <109533854+comet3456998765@users.noreply.github.com>
2023-01-29 08:28:41 -0600

    Update YesWorldHubOwners.lua

--- YesWorldHubOwners.lua
+++ YesWorldHubOwners.lua
@@ -1,8 +1,33 @@
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/juywvm/ui-libs/main/Akali_Notify_Library/AkaliNotifyLibrary"))();
+
+local Notify = AkaliNotif.Notify;
+
+Notify({
+    Description = " Exploits Support: Synapse, Script-Ware, Krnl ";
+    Title = " Loading Owner Version ";
+    Duration = 5;
+});
+wait("4")
+Notify({
+    Description = " Zeus, Electron, Comet 3, Fluxus, Oxygen U ";
+    Title = " Loading Owner Version ";
+    Duration = 4;
+});
+wait(3)
+Notify({
+    Description = " Krnl (No support adm for mobiles 2) ";
+    Title = " Loading Owner Version (5 Sec) ";
+    Duration = 3;
+});
+wait("5")
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 
 local win = Flux:Window("Net Hub (Owner)", "By ! World Hub#4124", Color3.fromRGB(255, 110, 48), Enum.KeyCode.M)
 local tab = win:Tab("Credits", "http://www.roblox.com/asset/?id=6023426915")
-tab:Label("Dev: ! World Hub#4124")
+tab:Label("Dev: Triangleex#3487")
+tab:Label("Interface: ! World Hub#4124")
+tab:Label("Scripter: ! World Hub#4124 ")
+tab:Label("Esconde y abre la gui con M/Toggle gui with M")
 tab:Button("Chatbypasser", "", function()
 	Flux:Notification("running", "Alright")
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Chatbypaser-v/main/YesWHub"))()
