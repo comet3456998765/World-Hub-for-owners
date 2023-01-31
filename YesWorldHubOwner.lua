b68420528dfca68a7e9a1ee2afc6ec36bb95b720
comet3456998765 <109533854+comet3456998765@users.noreply.github.com>
2023-01-31 15:18:50 -0600

    Update YesWorldHubOwner.lua

--- YesWorldHubOwner.lua
+++ YesWorldHubOwner.lua
@@ -1,267 +1,168 @@
-local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local Discord = "https://discord.gg/NYxgXzt8aK"
+function discord()
+setclipboard(Discord)
+end
+local TikTok = "https://www.tiktok.com/@sk_lvsmusic"
+function TikTok()
+setclipboard(TikTok)
+end
+local Credits = "https://github.com/comet3456998765/Credits/blob/main/README.md"
+function Credits()
+setclipboard(Credits)
+end
 
-local win = Flux:Window("Net Hub", "Version Owners", Color3.fromRGB(255, 110, 48), Enum.KeyCode.RightControl)
-local tab = win:Tab("Credits", "http://www.roblox.com/asset/?id=6023426915")
-tab:Label("Dev: ! World Hub#4124")
-tab:Label("Interface: ! World Hub#4124")
-tab:Label("Scripter: ! World Hub#4124 ")
-tab:Label("Adm for mobiles 2 creator: gramajoalexishack#0524")
-tab:Label("Creador de Adm for mobiles 2: gramajoalexishack#0524")
-tab:Label("Esconde y abre la gui con el control de la derecha/Toggle gui with RightControl")
-tab:Button("Chatbypasser", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Chatbypaser-v/main/YesWHub"))()
-end)
-tab:Button("Chat Spy", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/newscript/main/Yeschatspy"))()
-end)
-tab:Button("Antiafk", "", function()
-	Flux:Notification("running", "Alright")
-	local vu = game:GetService("VirtualUser")
+local Versions = "V1"
+
+local GUI = Mercury:Create{
+    Name = "Net Hub",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Legacy,
+    Link = "https://ww.Lvs_page/Net Hub.api/Hub.lua"
+}
+GUI:Notification{
+	Title = "Net Hub Alerta/Alert",
+	Text = "Esta es una version de prueba por favor esperar/This is a trial version please wait",
+	Duration = 5,
+	Callback = function() 
+    end
+}
+GUI:Notification{
+	Title = "Net Hub Alerta/Alert",
+	Text = "Version: V1 (Beta)", "..Versions",
+	Duration = 10,
+	Callback = function() 
+    end
+}
+
+local Tab = GUI:Tab{
+	Name = "Credits",
+	Icon = "rbxassetid://8569322835"
+}
+
+Tab:Button{
+	Name = "Copy Credits Link",
+	Description = nil,
+	Callback = function() 
+    setclipboard("https://github.com/comet3456998765/Credits/blob/main/README.md")
+    end
+}
+Tab:Button{
+	Name = "Copy Discord link",
+	Description = nil,
+	Callback = function() 
+    setclipboard(Discord)
+    end
+}
+Tab:Button{
+	Name = "Copy My TikTok link",
+	Description = nil,
+	Callback = function() 
+    setclipboard(TikTok)
+    end
+}
+Tab:Button{
+	Name = "ChatByPasser",
+	Description = "Use chat by passer: /s fuck",
+	Callback = function() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Chatbypaser-v/main/YesWHub"))()
+    end
+}
+Tab:Button{
+	Name = "Chat Spy",
+	Description = "toggle Chat spy: /spy",
+	Callback = function() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/newscript/main/Yeschatspy"))()
+    end
+}
+Tab:Button{
+	Name = "Antiafk",
+	Description = nil,
+	Callback = function() 
+    local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    wait(1)
    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
-end)
-local tab = win:Tab("Games", "http://www.roblox.com/asset/?id=6023426915")
-tab:Label("Prison Life")
-tab:Button("Adm for mobiles 2", "Adm for mobiles", function()
-	Flux:Notification("running Adm for mobiles 2", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/NewScript1/main/Yes"))()
-end)
-tab:Button("Adm for mobiles", "Mobile Adm for prison life", function()
-	Flux:Notification("running Adm for mobiles", "Alright")
-	loadstring(game:HttpGet('https://pastebin.com/raw/iZ64yzjE'))();
-end)
-tab:Button("Septex Adm (Needs Key)", "Septex Adm for pc", function()
-	Flux:Notification("running Septex Adm", "Alright")
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/XTheMasterX/Scripts/Main/PrisonLife'),true))()
-end)
-tab:Button("Tiger Adm", "Tiger Adm for pc", function()
-	Flux:Notification("running Tiger Adm", "Alright")
-	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/H17S32/Tiger_Admin/main/Script"))()
-end)
-tab:Button("Ymir Adm", "Ymir Adm for pc", function()
-	Flux:Notification("running Ymir Adm", "Alright")
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/senorkal/YmirAdmin/main/source'),true))()
-end)
-tab:Button("Silentaim for prison life", "Silentaim for prison life (pc)", function()
-	Flux:Notification("running Silentaim gui", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Comente/main/ForSKHUBKAVO%20LIB"))()
-end)
-tab:Button("Bang Gui", "Bang Gui for prison life (pc)", function()
-	Flux:Notification("running Bang Gui", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/script-for-SKhb/main/script"))()
-end)
-tab:Label("Universal")
-tab:Button("Owl Hub", "Owl Hub Script", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/OwlHub.txt"))()
-end)
-tab:Button("Eclipse Hub", "Eclipse Hub script", function()
-	Flux:Notification("running", "Alright")
-	getgenv().mainKey = "nil"
+    end
+}
+local Tab = GUI:Tab{
+	Name = "Universal",
+	Icon = "rbxassetid://8569322835"
+}
+Tab:Button{
+	Name = "Owl Hub",
+	Description = nil,
+	Callback = function() 
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/OwlHub.txt"))()
+    end
+}
+Tab:Button{
+	Name = "Eclipse Hub",
+	Description = nil,
+	Callback = function() 
+    getgenv().mainKey = "nil"
 
 local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
-end)
-tab:Button("HoHo Hub", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
-end)
-tab:Button("Ice Hub", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"))()
-end)
-tab:Button("Infinite Yield", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Infinite%20Yield.txt"))()
-end)
-tab:Button("CMD-X", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
-end)
-tab:Button("VG Hub", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/Testing-Grounds/main/Supported%20games%20and%20features"))()
-end)
-tab:Button("Dex Explorer", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/dex/main/dexscript"))()
-end)
-tab:Button("Turtle Spy", "", function()
-	Flux:Notification("running ", "Alright")
-	loadstring(game:HttpGet("https://pastebin.com/raw/BDhSQqUU", true))()
-end)
-tab:Label("Others games")
-tab:Button("Adm for brookhaven (Mobile)", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))();
-end)
-tab:Button("Survive the killer script", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/new-script/main/Main"))()
-end)
-tab:Button("Liberty Country Script", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Altraxs-Hub/main/Loader.lua", true))()
-end)
-tab:Button("Legends of speed Script", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/other-script/main/script"))()
-end)
-tab:Button("Dinosaur sim", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Xcom911/DS/main/HoneyBadger'))()
-end)
-tab:Button("Oaklands ore esp", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://rawscripts.net/raw/Oaklands-Ore-esp-8480"))()
-end)
-tab:Button("Jailbreak", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/BloxiYT/Diamond/main/AdoptMe"))()
-end)
-tab:Button("Steeps Steps", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLking123456/SteepSteps/main/Roblox2901"))()
-end)
-tab:Button("Streec Soccer", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/hussain1323232234/My-Scripts/main/Street%20Soccer'))()
-end)
-tab:Button("Strucid", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet'https://raw.githubusercontent.com/ao-0/methamphetamine-solutions/main/Loader.lua')("https://discord.gg/curPv7uhQc")
-end)
-tab:Button("Faded Main (Da Hood)", "", function()
-	Flux:Notification("running", "Alright")
-	_G.Toggles = "V" -- You can put any keybind
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NighterEpic/Faded/main/YesEpic", true))()
-end)
-tab:Button("Faded Silentaim (Da Hood)", "", function()
-	Flux:Notification("running", "Alright")
-	_G.HideToggle = "RightShift" -- You can put any keybind
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NighterEpic/FadedLock/main/YesEpic"))()
-end)
-tab:Button("Butter Hub (Lumber Tycoon 2)", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/butter-hub/main/test%20(2).txt'))()
-end)
-tab:Button("Evade", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Hydra/main/HYDRA%20LIB"))()
-end)
-tab:Button("Muscle Legends", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Soffware/3kTMd/main/HadesHub.lua"),true))()
-end)
-tab:Button("Arsenal", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Main", true))()
-end)
-tab:Button("KAT", "", function()
-	Flux:Notification("running", "Alright")
-	if game.PlaceId == 621129760 then
-        loadstring(game:HttpGet("https://pastebin.com/raw/78kG7trR", true))()
-         
-        -- Gui to Lua
-        -- Version: 3.2
-         
-        -- Instances:
-         
-        local ScreenGui = Instance.new("ScreenGui")
-        local Frame = Instance.new("Frame")
-        local TextLabel = Instance.new("TextLabel")
-        local TextButton = Instance.new("TextButton")
-         
-        --Properties:
-         
-        ScreenGui.Parent = game.CoreGui
-        ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-         
-        Frame.Parent = ScreenGui
-        Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-        Frame.BackgroundTransparency = 0.300
-        Frame.BorderColor3 = Color3.fromRGB(255, 0, 0)
-        Frame.BorderSizePixel = 5
-        Frame.Position = UDim2.new(0, 0, 0.5, -62)
-        Frame.Size = UDim2.new(0, 702, 0, 124)
-        Frame.Active = true
-         
-         
-        TextLabel.Parent = Frame
-        TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        TextLabel.BackgroundTransparency = 3.000
-        TextLabel.Size = UDim2.new(0, 702, 0, 124)
-        TextLabel.Font = Enum.Font.SourceSans
-        TextLabel.Text = "This GUI is not the best with Every exploit. It's mainly for paid exploits, such as Script ware and Synapse, If you dont have a paid exploit, then I Suggest You Use Krnl Or Fluxus, Press L To Toggle The UI. I recommend having at least laser when using silent aim with a knife, incase you dont have try out spam knife!"
-        TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-        TextLabel.TextScaled = true
-        TextLabel.TextSize = 14.000
-        TextLabel.TextWrapped = true
-         
-        TextButton.Parent = Frame
-        TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        TextButton.BackgroundTransparency = 3.000
-        TextButton.Position = UDim2.new(0.928774953, 0, 0.669354856, 0)
-        TextButton.Size = UDim2.new(0, 42, 0, 41)
-        TextButton.Font = Enum.Font.SourceSans
-        TextButton.Text = "Close"
-        TextButton.TextColor3 = Color3.fromRGB(255, 0, 0)
-        TextButton.TextScaled = true
-        TextButton.TextSize = 50.000
-        TextButton.TextWrapped = true
-        TextButton.MouseButton1Down:connect(function()
-            Frame.Visible = false
-        end)
-         
-         
-         
-         
-        end
-end)
-tab:Button("Xeny Ware (MM2)", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/AdvancedWare/VScript/main/Script", true))()
-end)
-tab:Button("Vynixus (MM2)", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
-end)
-tab:Button("MM2 script (Mobile)", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Drifter0507/GUIS/main/MURDER%20MYSTERY%202", true))();
-end)
-local tab = win:Tab("Fe Scripts", "http://www.roblox.com/asset/?id=6023426915")
-tab:Label("Fe scripts")
-tab:Button("CloVr", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Clovr-for-SKHub-Kavo-and-Orion/main/O"))()
-end)
-tab:Button("Nameless", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/Script-for-skhub/main/Name"))()
-end)
-tab:Button("Fe psycho slasher (Needs Slasher Sword)", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet(('https://pastefy.ga/iGyVaTvs/raw'),true))()
-end)
-tab:Label("Fe scripts hubs")
-tab:Button("Pendulum V5", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Tescalus/Pendulum-Hubs-Source/main/Pendulum%20Hub%20V5.lua"))()
-end)
-tab:Button("Universal Fe", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Dvrknvss/UniversalFEScriptHub/main/Script'))()
-end)
-tab:Button("Shadows Hub", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/other-script-V/main/loading%3Av"))()
-end)
-tab:Button("1x1x1x1 Gui", "", function()
-	Flux:Notification("running", "Alright")
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/tyreltrijo/1x1/main/1x1x1x1'))()
-end)
+    end
+}
+Tab:Button{
+	Name = "HoHo Hub",
+	Description = nil,
+	Callback = function() 
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+    end
+}
+Tab:Button{
+	Name = "Ice Hub",
+	Description = nil,
+	Callback = function() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"))()
+    end
+}
+Tab:Button{
+	Name = "Infinite Yield",
+	Description = nil,
+	Callback = function() 
+    loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Infinite%20Yield.txt"))()
+    end
+}
+Tab:Button{
+	Name = "CMD-X",
+	Description = nil,
+	Callback = function() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+    end
+}
+Tab:Button{
+	Name = "VG Hub",
+	Description = nil,
+	Callback = function() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/Testing-Grounds/main/Supported%20games%20and%20features"))()
+    end
+}
+local Tab = GUI:Tab{
+	Name = "Games All Gui",
+	Icon = "rbxassetid://8569322835"
+}
+Tab:Button{
+	Name = "Run Gui",
+	Description = nil,
+	Callback = function() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/All-games-new-/main/Yesgui"))()
+    end
+}
+
+local Tab = GUI:Tab{
+	Name = "All fe scripts gui",
+	Icon = "rbxassetid://8569322835"
+}
+
+Tab:Button{
+	Name = "Run All Fe scripts gui",
+	Description = nil,
+	Callback = function() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/comet3456998765/All-games-new-/main/YES"))()
+    end
+}
